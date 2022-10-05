38b864c2a08f20c9a9e8265e9cf03a765464efba
jovellabay <jovellabay@gmail.com>
2022-10-05 11:11:29 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Autoupdater Demo v 0.1.0
-				</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Autoupdater Demo v 0.1.3</p>
+        <h1>This is the newest update with manual update query</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
